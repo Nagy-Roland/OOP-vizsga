68087911bafc883482c4eb46cc61e7ee9e5a9b4c
Nagy-Roland <kisrobert612@gmail.com>
2025-11-03 10:44:12 +0200

    f

---
+++ src/main/java/ro/umfst/vizsga/Identifiable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.umfst.vizsga;
+
+/**
+ *
+ * @author Laptop
+ */
+public interface Identifiable {
+    
+    String getId();
+    String businessKey();
+}
+
