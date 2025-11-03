8a50a1cbab9dc2b5e9203586b233b18db99c0a76
Nagy-Roland <kisrobert612@gmail.com>
2025-11-03 10:43:40 +0200

    f

---
+++ src/main/java/ro/umfst/vizsga/VizsgaFrame.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.umfst.vizsga;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+import com.fasterxml.jackson.databind.*;
+
+/**
+ *
+ * @author Laptop
+ */
+public class VizsgaFrame extends JFrame{
+    
+}
