d2f5006917d7643eab1d2ae41146570bbf109534
Nagy-Roland <kisrobert612@gmail.com>
2025-11-03 09:07:55 +0200

    f

---
+++ src/main/java/ro/umfst/vizsga/Vizsga.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ro.umfst.vizsga;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+import com.fasterxml.jackson.databind.*;
+/**
+ *
+ * @author Laptop
+ */
+public class Vizsga {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
